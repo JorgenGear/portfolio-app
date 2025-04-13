ca7311c73739e59079e96cc8d5baa1e64439fa10
Rhett Jorgensen <121641690+JorgenGear@users.noreply.github.com>
2025-04-12 21:27:45 -0600

    deps

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,13 +2,14 @@ import { notFound } from 'next/navigation';
 import { getBlogPost } from '@/lib/blog';
 import BlogPostContent from './BlogPostContent';
 
-interface PageProps {
+interface Props {
     params: {
         slug: string;
     };
+    searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function BlogPost({ params }: PageProps) {
+export default async function BlogPost({ params }: Props) {
     const post = await getBlogPost(params.slug);
 
     if (!post) {
